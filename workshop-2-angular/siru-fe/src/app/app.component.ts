fa68a67076632c59795097dfb2fb4fad48d3976f
JollyMick <scarpa.michele.90@gmail.com>
2024-01-08 22:16:12 +0100

    libreria e prima configurazione

--- workshop-2-angular/siru-fe/src/app/app.component.ts
+++ workshop-2-angular/siru-fe/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+
+import { OAuthService } from 'angular-oauth2-oidc';
+
+import { authConfig } from './auth-config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'siru-fe';
+  
+  constructor(private oauthService: OAuthService) {
+
+  }
+  
+  ngOnInit(): void {
+    this.oauthService.configure(authConfig);
+
+    this.oauthService.loadDiscoveryDocumentAndLogin()
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((error) => {
+        console.warn(error)
+      })
+  }
+
+
 }
--- workshop-2-angular/siru-fe/src/app/app.module.ts
+++ workshop-2-angular/siru-fe/src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
